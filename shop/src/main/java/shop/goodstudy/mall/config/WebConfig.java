5d4d60082f35262aeac3e7139399a30e0122cc62
thsong10 <thsong10@neonexsoft.com>
2019-03-15 16:06:30 +0900

    CORS 설정 변경, 파비콘 경로 설정 변경

--- shop/src/main/java/shop/goodstudy/mall/config/WebConfig.java
+++ shop/src/main/java/shop/goodstudy/mall/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/auth/**");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200");
             }
         };
     }
